1bb78d94d90c8f7f995dba20a03d1993fd5c502f
paulo-24 <jpabrajano81@gmail.com>
2025-02-20 21:52:38 +0800

    added new route

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Hero from "../components/Hero";
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      {/* Other sections like About, Projects, etc. */}
+    </div>
+  );
+};
+
+export default Home;
