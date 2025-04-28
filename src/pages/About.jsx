b7f4cd3e0eaf8ab091026a075d9163c87f73c5b4
paulo-24 <jpabrajano81@gmail.com>
2025-04-28 11:48:59 +0800

    Updated About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@
 // About.jsx
 import React from 'react';
 import './About.css';
-import AboutMe from './Aboutme';
+import AboutMe from './AboutMe';
 import Skills from './Skills';
 import Tools from './Tools';
 import Interests from './Interests';
